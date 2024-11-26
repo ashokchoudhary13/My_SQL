1d38ee50e4d5cab620fb7358cacfde2558318b31
Ashok Choudhary <138196197+ashokchoudhary13@users.noreply.github.com>
2024-11-26 20:50:26 +0530

    Add files via upload
    run this file in your MySQL to get the tables in your database and start your practicing too !!

---
+++ tables used for  practicing query's.sql
@@ -0,0 +1,40 @@
+CREATE TABLE emp (
+    empno INT PRIMARY KEY, -- Employee number
+    ename VARCHAR(50),     -- Employee name
+    job VARCHAR(50),       -- Job title
+    mgr INT,               -- Manager's employee number (nullable)
+    hiredate DATE,         -- Date of hire
+    sal DECIMAL(10, 2),    -- Salary
+    comm DECIMAL(10, 2),   -- Commission (nullable)
+    deptno INT             -- Department number
+);
+
+INSERT INTO emp (empno, ename, job, mgr, hiredate, sal, comm, deptno) VALUES
+(7369, 'NITIN', 'CLERK', 7902, '1980-12-17', 800.00, NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.00, 300.00, 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250.00, 500.00, 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975.00, NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250.00, 1400.00, 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850.00, NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450.00, NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', 3000.00, NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000.00, NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500.00, 750.00, 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100.00, NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950.00, NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000.00, NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300.00, NULL, 10);
+ 
+ 
+ CREATE TABLE dept (
+    deptno INT PRIMARY KEY,      -- Department number
+    dname VARCHAR(50),           -- Department name
+    loc VARCHAR(50)              -- Location
+);
+
+
+INSERT INTO dept (deptno, dname, loc) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
